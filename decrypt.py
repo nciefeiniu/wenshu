5adbd84a2ef657264e1342788e0ecc0c4d500d4e
feiniu <tao.liu@epmap.org>
2020-04-16 09:44:16 +0800

    perf: update to python 3

--- decrypt.py
+++ decrypt.py
@@ -1,21 +1,21 @@
-import base64
 import re
-import urlparse as parse
+import base64
+
+from urllib.parse import urljoin
 
 
 _pattern = re.compile(r"dynamicurl\|(?P<path>.+?)\|wzwsquestion\|(?P<question>.+?)\|wzwsfactor\|(?P<factor>\d+)")
 
 
-def decrypt_wzws(text):
+def decrypt_wzws(text: str) -> str:
     # noinspection PyBroadException
     try:
         return _decrypt_by_python(text)
     except Exception:
-        pass
-        # return _decrypt_by_nodejs(text)
-        
+        print("解析html错误")
 
-def _decrypt_by_python(text):
+
+def _decrypt_by_python(text: str) -> str:
     base_url = "http://wenshu.court.gov.cn"
 
     group_dict = _pattern.search(text).groupdict()
@@ -26,15 +26,13 @@ def _decrypt_by_python(text):
     label = "WZWS_CONFIRM_PREFIX_LABEL{}".format(sum(ord(i) for i in question) * factor + 111111)
     challenge = base64.b64encode(label.encode()).decode()
 
-    dynamic_url = parse.urljoin(base_url, path)
+    dynamic_url = urljoin(base_url, path)
     dynamic_url = "{url}?{query}".format(url=dynamic_url, query="wzwschallenge={}".format(challenge))
     return dynamic_url
     
    
 if __name__ == "__main__":
     with open("demo.html") as f:
-    _content = f.read()
-
+        _content = f.read()
     _resp = decrypt_wzws(_content)
-
-    print _resp
+    print(_resp)
